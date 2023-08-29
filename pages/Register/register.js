7fae5ec2aec37afc3c55553255fe143ec8d74908
Luka Janelidze <lukajanelidze5@gmail.com>
2023-08-29 23:50:04 +0400

    feat: handle registration

--- pages/Register/register.js
+++ pages/Register/register.js
@@ -10,45 +10,60 @@ let emailValid = false;
 let passwordValid = false;
 let confirmPasswordValid = false;
 
-const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 createButton.addEventListener("click", () => {
-
-  if (!emailRegex.test(emailInput.value)) {
+ 
+  if (!emailInput.value) {
     emailInput.style.border = "1px solid red";
     emailError.innerText = "Can't be empty";
     emailValid = false;
-
   } else {
-    emailInput.style.border = "";
-    emailError.innerText = "";
-    emailValid = true;
-  };
+    if (!emailRegex.test(emailInput.value)){
+      emailInput.style.border = "1px solid red";
+      emailError.innerText = "Invalid email";
+      emailValid = false;
+    } else {
+      emailInput.style.border = "";
+      emailError.innerText = "";
+      emailValid = true;
+    }
+  }
 
-  if(passwordInput.value.length < 8) {
+  if (passwordInput.value.length < 8) {
     passwordInput.style.border = "1px solid red";
     passwordError.innerText = "Please check again";
     passwordValid = false;
   } else {
-    passwordInput.style.border = ""
-    passwordError.innerText = ""
+    passwordInput.style.border = "";
+    passwordError.innerText = "";
     passwordValid = true;
-    
-  };
+  }
 
-  if(passwordInput.value != confirmPasswordInput.value) {
+  if (passwordInput.value != confirmPasswordInput.value) {
     confirmPasswordInput.style.border = "1px solid red";
     confirmPasswordError.innerText = "Passwords do NOT match";
     confirmPasswordValid = false;
   } else {
-    confirmPasswordInput.style.border = ""
-    confirmPasswordError.innerText = ""
+    confirmPasswordInput.style.border = "";
+    confirmPasswordError.innerText = "";
     confirmPasswordValid = true;
-    
-  };
+  }
 
+ if (emailValid && passwordValid && confirmPasswordValid) {
+   
+    const prevUsers = JSON.parse(localStorage.getItem("users"));
+    if(prevUsers){
+      localStorage.setItem("users", JSON.stringify([
+        ...prevUsers,
+        { email: emailInput.value, password: passwordInput.value },
+      ]));
+    } else {
+      localStorage.setItem("users", JSON.stringify([
+        { email: emailInput.value, password: passwordInput.value },
+      ]));
+    }
 
-  if (emailValid && passwordValid && confirmPasswordValid) {
     createButton.setAttribute("href", "../../index.html");
   }
 });
