5a5bc23205238465434bb2db97efa87e7248fa3a
David <davidmikeladze00@gmail.com>
2023-09-01 15:09:27 +0400

    feat: get value from local storage

---
+++ pages/Preview/preview.js
@@ -0,0 +1,5 @@
+const storedProfileImg = localStorage.getItem("profileImg");
+const storedFirstName = localStorage.getItem("firstName");
+const storedlastName = localStorage.getItem("lastName");
+const storedUserEmail = localStorage.getItem("userEmail");
+const storedLinks = localStorage.getItem("links");
